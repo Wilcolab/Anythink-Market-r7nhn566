54bf5b9c5057730acce46dae51a6e932b473da7c
Swapnil Satpathy <swapnil.satpathy@students.iiit.ac.in>
2025-07-07 19:25:38 +0000

    Take port from process as well

--- js-server/index.js
+++ js-server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 8001;
+const PORT = process.env.PORT || 8001;
 
 const tasks = ["Write a diary entry from the future", "Create a time machine from a cardboard box", "Plan a trip to the dinosaurs", "Draw a futuristic city", "List items to bring on a time-travel adventure"];
 
