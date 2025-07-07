075beb91fc3d29aea74a16c8b30db7ea17fd5076
Swapnil Satpathy <swapnil.satpathy@students.iiit.ac.in>
2025-07-07 19:29:18 +0000

    Final check

--- js-server/index.js
+++ js-server/index.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 8001;
 
 const tasks = ["Write a diary entry from the future", "Create a time machine from a cardboard box", "Plan a trip to the dinosaurs", "Draw a futuristic city", "List items to bring on a time-travel adventure"];
 
+app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
