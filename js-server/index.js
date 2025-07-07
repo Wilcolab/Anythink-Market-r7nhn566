447f15286b16c4dba07c62cc7a0f479b4631c862
Swapnil Satpathy <swapnil.satpathy@students.iiit.ac.in>
2025-07-07 18:51:00 +0000

    Add routes

--- js-server/index.js
+++ js-server/index.js
@@ -2,6 +2,24 @@ const express = require('express');
 const app = express();
 const PORT = 8001;
 
+const tasks = ["Write a diary entry from the future", "Create a time machine from a cardboard box", "Plan a trip to the dinosaurs", "Draw a futuristic city", "List items to bring on a time-travel adventure"];
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.post("/tasks", (req, res) => {
+    const { text } = req.body;
+    tasks.push(text);
+    res.send({"message": "Task added successfully"});
+});
+
+app.get("/tasks", (req, res) => {
+    res.send({"tasks": tasks});
+});
+
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
